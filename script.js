661f0d74bb46f473173cc3bb51aeb1f583f3f49f
martin cocchi nan <martin.cocchi@gmail.com>
2026-09-17 18:04:50 -0300

    added basic const linking the html ids and classes to work on to the script file

---
+++ script.js
@@ -0,0 +1,9 @@
+const numeroRBG = document.getElementById("generate");
+
+const count = document.getElementsByClassName("palette-created");
+const hslValue = document.getElementsByClassName("hsl-value");
+
+const buttonSix = document.getElementById("amount-of-colors-6");
+const buttonEight = document.getElementById("amount-of-colors-8");
+const buttonNine = document.getElementById("amount-of-colors-9");
+const paletteContainer = document.getElementById("palette-code");
