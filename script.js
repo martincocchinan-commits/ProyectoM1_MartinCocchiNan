455b4fac13fa9c20b0d3a1747d9933aaab7608ff
martin cocchi nan <martin.cocchi@gmail.com>
2026-09-17 18:17:08 -0300

    added the starting clones when the web starts, the DOMContentLoaded function to start with 6 clones, the hsl color format on start of the web with function initRandomColors for the colors to be random.

--- script.js
+++ script.js
@@ -8,24 +8,43 @@ const buttonEight = document.getElementById("amount-of-colors-8");
 const buttonNine = document.getElementById("amount-of-colors-9");
 const paletteContainer = document.getElementById("palette-code");
 
-function obtenerRGB(r, g, b) {
-       const red = Math.floor(Math.random() * 256);
-       const green = Math.floor(Math.random() * 256);
-       const blue = Math.floor(Math.random() * 256);
 
-       return `rgb(${red}, ${green}, ${blue})`;
+// starting web
+function initClones(maxClones) {
+    document.querySelectorAll(".palette-clone").forEach(el => el.remove());
+
+    for (let i = 0; i < maxClones; i++) {
+        const clone = paletteContainer.cloneNode(true);
+        clone.removeAttribute("id");
+        clone.classList.add("palette-clone");
+
+        paletteContainer.parentNode.appendChild(clone);
+    }
 }
 
-numeroRBG.addEventListener("click", function (event) {
-       console.log("target:", event.target);
-       console.log("currentTarget:", event.currentTarget);
-
-       for (let i = 0; i < count.length; i++) {
-
-              const nuevoColor = obtenerRGB();
-
-              count[i].style.backgroundColor = nuevoColor;
-              RGBvalue[i].textContent = nuevoColor;
-       }
+// Run automatically once the page loads
+document.addEventListener("DOMContentLoaded", function () {
+    initClones(5);
 });
-*/
+
+// HSL ON START
+function obtenerHsl() {
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = Math.floor(Math.random() * 100);
+    const light = Math.floor(Math.random() * 100);
+
+    return `hsl(${hue}, ${saturation}%, ${light}%)`;
+}
+
+function initRandomColors() {
+    for (let i = 0; i < count.length; i++) {
+        const newColor = obtenerHsl();
+
+        count[i].style.backgroundColor = newColor;
+        hslValue[i].textContent = newColor;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    initRandomColors();
+});
